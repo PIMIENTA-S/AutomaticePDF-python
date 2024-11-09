4c2282487f52c8215327f41ae4621e78ea388cb2
Angell Pimienta's <angelefrain1510@gmail.com>
2024-11-08 23:58:45 -0500

    commit error

--- Extract/scripts.py
+++ Extract/scripts.py
@@ -1,4 +1,4 @@
-import PyPDF2 as pdf
+import PyPDF2 as pd
 
 # Se crea un objeto para leer el pdf
 # Toma dos parametros, la ruta y el modo de lectura, en este caso lectura binaria(rb)
@@ -9,7 +9,7 @@ import PyPDF2 as pdf
 
 path = 'Horario_del_Alumno.pdf'
 
-pdf_reader = pdf.PdfReader(path)
+pdf_reader = pd.PdfReader(path)
 page = pdf_reader.pages[0]
 
 # text = page.extract_text().replace('\n', ' ').split(' ')
@@ -48,3 +48,16 @@ def horarioSort(text):
 print(horarioSort(text))
 # print(pdf_reader.getNumPages())
 
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "d:\Projects\AutomaticePDF-python\Extract\scripts.py", line 12, in <module>
+#     pdf_reader = pd.PdfReader(path)
+#                  ^^^^^^^^^^^^^^^^^^
+#   File "D:\Projects\AutomaticePDF-python\Extract\.venv\Lib\site-packages\PyPDF2\_reader.py", line 319, in __init__
+#     self.read(stream)
+#   File "D:\Projects\AutomaticePDF-python\Extract\.venv\Lib\site-packages\PyPDF2\_reader.py", line 1426, in read
+#     self._read_xref_tables_and_trailers(stream, startxref, xref_issue_nr)
+#   File "D:\Projects\AutomaticePDF-python\Extract\.venv\Lib\site-packages\PyPDF2\_reader.py", line 1632, in _read_xref_tables_and_trailers
+#     raise PdfReadError(f"trailer can not be read {e.args}"
